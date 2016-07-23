0bc1601b78cc1ee126a5d6e16fc36eb2164d3912
jgrom <jgromdev@gmail.com>
2016-07-23 22:33:49 +0200

    Exercice --terminé

--- ex_03.rb
+++ ex_03.rb
@@ -8,7 +8,7 @@ puts "Roosters #{100 - 25  * 3 % 4}"        # 97
 puts "Now I will count the eggs:"
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1.to_f / 4 + 6 # 6.75
 
-# Write the result of the compararison between two calculations
+# Write the result of the comparison between two calculations
 puts "Is it true that 3 + 2 < 5 - 7?"
 puts 3 + 2 < 5 - 7                          # false
 
@@ -19,7 +19,7 @@ puts "What is 5 - 7? #{5 - 7}"              # -2
 puts "Oh, that's why it's false."
 puts "How about some more."
 
-# Write the result of the compararisons between the two values
+# Write the result of the comparisons between the two values
 puts "Is it greater? #{5 > -2}"             # true
 puts "Is it greater or equal? #{5 >= -2}"   # true
 puts "Is it less or equal ? #{5 <= -2}"     # false
