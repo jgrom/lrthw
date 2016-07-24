593e52daee992c650913de780531598bc6abac56
jgrom <jgromdev@gmail.com>
2016-07-24 19:42:28 +0200

    Delete Ex_12-sd.rb

--- Ex_12-sd.rb
+++
@@ -1,5 +0,0 @@
-print "Amount $$$ : "
-amount = gets.chomp.to_f
-
-puts "Give back (10%) : $#{amount / 10}"  # 103.4 => 10.34 :ok
-puts "Give back (10%) : $#{amount * 0.1}" # 103.4 => 10.340000000000002 :??
